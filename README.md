90ee9d886e5a251f2efc85834f6c91d14a8aeb57
root <root@DESKTOP-2KU5GC4.localdomain>
2019-06-04 14:34:22 +0300

    edited readme

--- README.md
+++ README.md
@@ -2,8 +2,18 @@
 A small demo on the use of socket io and express to make a simple chatting app
 
 to run it simply type the following commands in the terminal.
+
+
 git clone https://github.com/Poufy/chattingapp.git
+
+
 npm install --save express
+
+
 npm install --save socket.io
+
+
 node index.js
+
+
 and open your browser on http://localhost:3000/
--- index.js
+++ index.js
@@ -11,11 +11,14 @@ app.get('/', function(req, res){
 //built in connection event when the event occurs that means a socket object has been passed to the server
 //and print to the console that a user has joined.
 io.on('connection', function(socket){
-//console.log('new user joined');
-//here we send an event to every socket connected to the server that has the name 'joined'
-//this event is handled in every client to update their screen and print that a new user has joined the chat
+//here we handle the emition of the userName from each client and add them to a map that has their UNIQUE socket id and 
+	socket.on('userID', function(userName){
+		connectedUsers.set(socket.id, userName);
+	});
+	//here we send an event to every socket connected to the server that has the name 'joined'
+	//this event is handled in every client to update their screen and print that a new user has joined the chat
 	//name of the user joined has to be handled here. it should be sent as a string to other clients.
-	io.emit('joined');
+	io.emit('joined', 'a user ');
 //here we handle the emit from the client to the server. we take the message from the client that sent it and emit it to all other connected sockets.
 	socket.on('chat message', function(msg){
 		console.log("chat message: " + msg);
@@ -23,15 +26,12 @@ io.on('connection', function(socket){
 		io.emit('chat message', msg);
 	});
 	socket.on('disconnect', function(){
-		//here we emit the event called 'disconnect' and it is handled in every client.
-		io.emit('disconnect');
+		//here we emit the event called 'disconnect' and the user name of the disconnected node that we have stored in our map.
+		//and it is handled in every client.
+
+		io.emit('disconnect', connectedUsers.get(socket.id));
 	});
-	//here we handle the emition of the userName from each client and add them to a map that has their UNIQUE socket id and 
-	socket.on('userID', function(userName){
-		connectedUsers.set(socket.id, userName);
-		console.log(socket.id);
-		console.log(connectedUsers.get(socket.id));
-	});
+	
 });
 
 http.listen(port, function(){
